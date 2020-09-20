93bb93193b6263601a102547a948f03bf9aa3da7
Vikram K <vtgtyc@gmail.com>
2020-09-20 13:53:03 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './Assets/nh_logo.gif';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={12}>
+          <header>
+            <Nav
+              variant="tabs"
+              defaultActiveKey="/home"
+            >
+              <Nav.Link eventKey="logo">
+                <img src={logo} className="App-logo" alt="logo" />
+              </Nav.Link>
+              <Nav.Link eventKey="home">Home</Nav.Link>
+              <Nav.Link eventKey="new">New</Nav.Link>
+              <Nav.Link eventKey="comments">Comments</Nav.Link>
+              <Nav.Link eventKey="ask">Ask</Nav.Link>
+            </Nav>
+          </header>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-md-center">
+
+      </Row>
+    </Container>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
