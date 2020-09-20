ccc5cd8a92e7986f65b1b6b543146b606a8f30ad
Vikram K <vtgtyc@gmail.com>
2020-09-20 17:44:08 +0530

    scss inclusions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import logo from './Assets/nh_logo.gif';
-import './App.css';
+import './App.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
